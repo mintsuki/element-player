6bbfa513be24feb55f94c0a1184e91398a5309bb
mintsuki <mintsuki@protonmail.com>
2020-07-16 02:37:21 +0200

    Rebranding

--- install.sh
+++ install.sh
@@ -12,26 +12,26 @@ PREFIX="/usr/local"
 rm -rf build && mkdir build && cd build
 cmake ../generic-webplayer \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DPLAYER_NAME="riot-player" \
-    -DPLAYER_NICE_NAME="Riot Player" \
-    -DPLAYER_WEBAPP_URL="https://riot.im/app/"
+    -DPLAYER_NAME="element-player" \
+    -DPLAYER_NICE_NAME="Element Player" \
+    -DPLAYER_WEBAPP_URL="https://app.element.io/"
 make
 make install
 cd ..
 
 install -d "${PREFIX}/share/applications"
-cat <<EOF >"${PREFIX}/share/applications/riot-player.desktop"
+cat <<EOF >"${PREFIX}/share/applications/element-player.desktop"
 [Desktop Entry]
-Name=Riot Player
-Comment=Qtwebengine wrapper for Riot, because electron sucks.
+Name=Element Player
+Comment=Qtwebengine wrapper for Element, because electron sucks.
 GenericName=Internet Messenger
-Exec=$PREFIX/bin/riot-player
-Icon=$PREFIX/share/icons/riot-player.icon
+Exec=$PREFIX/bin/element-player
+Icon=$PREFIX/share/icons/element-player.icon
 Terminal=false
 Type=Application
 Categories=Network;InstantMessaging;
 EOF
-chmod 644 "${PREFIX}/share/applications/riot-player.desktop"
+chmod 644 "${PREFIX}/share/applications/element-player.desktop"
 
 install -d "${PREFIX}/share/icons"
-install -m 644 riot-player.icon "${PREFIX}/share/icons/riot-player.icon"
+install -m 644 element-player.icon "${PREFIX}/share/icons/element-player.icon"
